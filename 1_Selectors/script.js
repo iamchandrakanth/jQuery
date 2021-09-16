e95e4af853be26aa46e6fbdf80413daa28e74551
iamchandrakanth <ccmchandrakanth5@gmail.com>
2021-09-16 07:58:39 +0530

    jQuery Stuff

---
+++ 1_Selectors/script.js
@@ -0,0 +1,17 @@
+        // $("h1").hide();
+        // $("h1#heading1").hide();
+        // $("h1.heading2").hide();
+
+        // $('p span').css('color', 'red')
+        // $('ul#list li:first').css('color', 'red')
+        // $('ul#list li:last').css('color', 'blue');
+        // $('ul#list li:even').css('background-color', 'gray')
+        // $('ul#list li:odd').css('background-color', 'purple');
+        // $('ul#list li:nth-child(3n)').css('list-style', 'none')
+        // $(':button').hide();
+        // $(':submit').hide();
+        // $(':text').hide();
+
+        // $('[href]').css('color', 'red')
+        // $('[href="http://test.com"]').css('color', 'green');
+        // $('*').hide();
